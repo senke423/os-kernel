835014c97c98ef8678535610bf30f32da6932790
Lazar Senovic <lazar_senovic@proton.me>
2024-08-07 18:01:49 +0200

    Implemented syscall_c.c
    Implemented the C ABI wrapper. Also removed ./build from git tracking.

--- build/src/main.d
+++
@@ -1,9 +0,0 @@
-build/src/main.o: src/main.cpp src/../h/MemoryAllocator.hpp \
- src/../h/../lib/hw.h src/../h/LinkedList.hpp src/../h/../lib/mem.h \
- src/../h/../lib/console.h src/../h/riscv.hpp
-src/../h/MemoryAllocator.hpp:
-src/../h/../lib/hw.h:
-src/../h/LinkedList.hpp:
-src/../h/../lib/mem.h:
-src/../h/../lib/console.h:
-src/../h/riscv.hpp:
